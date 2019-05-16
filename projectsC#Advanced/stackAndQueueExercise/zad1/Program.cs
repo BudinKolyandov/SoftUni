85f4580336e1f62f53e1950b5d8ad1f21653fef9
BudinKolyandov <budin_kolyandov@abv.bg>
2019-05-16 14:49:26 +0300

    Finished first task

--- projectsC#Advanced/stackAndQueueExercise/zad1/Program.cs
+++ projectsC#Advanced/stackAndQueueExercise/zad1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace zad1
 {
@@ -6,7 +8,53 @@ namespace zad1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string [] stackRules = Console.ReadLine().Split().ToArray();
+            int [] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+            int n = int.Parse(stackRules[0]);
+            int s = int.Parse(stackRules[1]);
+            int x = int.Parse(stackRules[2]);
+
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(input[i]);
+            }
+            if (stack.Count > s)
+            {
+                for (int i = 0; i < s; i++)
+                {
+                    stack.Pop();
+                }
+            }
+            else
+            {
+                stack.Clear();
+            }
+            if (stack.Count>0)
+            {
+                if (stack.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    int minNumber = int.MaxValue;
+                    while (stack.Count != 0)
+                    {
+                        int current = stack.Peek();
+                        if (current < minNumber)
+                        {
+                            minNumber = current;
+                        }
+                        stack.Pop();
+                    }
+                    Console.WriteLine(minNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
         }
     }
 }
