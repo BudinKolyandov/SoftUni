484ce6cebd0419bf09379041c0b5ff1fef6ba268
BudinKolyandov <budin_kolyandov@abv.bg>
2019-05-18 18:36:31 +0300

    Added solutions for exercises 6 and 7.

---
+++ projectsC#Advanced/stackAndQueueExercise/Exercise 7/TruckTour.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise_7
+{
+    class TruckTour
+    {
+        static void Main(string[] args)
+        {
+            int firstPump = 0;
+            int fuelLeft = 0;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                List<int> currentPump = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+                int petrol = currentPump[0];
+                int distance = currentPump[1];
+
+                fuelLeft += petrol;
+
+                if (fuelLeft >= distance)
+                {
+                    fuelLeft -= distance;
+                }
+                else
+                {
+                    firstPump = i + 1;
+                    fuelLeft = 0;
+                }
+            }
+
+            Console.WriteLine(firstPump);
+
+        }
+    }
+}
---
+++ projectsC#Advanced/stackAndQueueExercise/Exercise6/AutoRepairAndService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise6
+{
+    class AutoRepairAndService
+    {
+        static void Main(string[] args)
+        {
+            string [] input = Console.ReadLine().Split(' ').ToArray();
+            List<string> servedVechicles = new List<string>();
+            Queue<string> queue = new Queue<string>(input);
+            string commands = string.Empty;
+            while ((commands = Console.ReadLine()) != "End")
+            {
+                string[] command = commands.Split('-').ToArray();
+                string currentCommand = command[0];
+                if (currentCommand == "Service" && queue.Count > 0)
+                {
+                    servedVechicles.Add(queue.Peek());
+                    Console.WriteLine($"Vehicle {queue.Dequeue()} got served.");
+                }
+                if (currentCommand == "CarInfo")
+                {
+                    string car = command[1];
+                    if (queue.Contains(car))
+                    {
+                        Console.WriteLine("Still waiting for service.");
+                    }
+                    if (servedVechicles.Contains(car))
+                    {
+                        Console.WriteLine("Served.");
+                    }
+                }
+                if (currentCommand == "History")
+                {
+                    servedVechicles.Reverse();
+                    Console.WriteLine(string.Join(", ", servedVechicles));
+                    servedVechicles.Reverse();
+                }
+                
+            }
+            if (queue.Count > 0)
+            {
+                Console.WriteLine($"Vehicles for service: {string.Join(", ", queue)}");
+            }
+            servedVechicles.Reverse();
+            Console.WriteLine($"Served vehicles: {string.Join(", ", servedVechicles)}");
+        }
+    }
+}
