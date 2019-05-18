4c84562b18162a43d42f091bb4be363ea4f3c285
BudinKolyandov <budin_kolyandov@abv.bg>
2019-05-18 17:33:35 +0300

    Changed Exercise5.cs because one of the tests was not correct.
    Signed-off-by: BudinKolyandov <budin_kolyandov@abv.bg>

---
+++ projectsC#Advanced/stackAndQueueExercise/ConsoleApp1/Exercise5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Exercise5
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+            int racks = 1;
+            int currentSum = 0;
+            Stack<int> stack = new Stack<int>(input);
+            int length = stack.Count;
+            for (int i = 0; i < length; i++)
+            {
+                int value = stack.Peek();
+                
+                if (currentSum + value <= capacity)
+                {
+                    currentSum += stack.Pop();
+                }
+                else
+                {
+                    currentSum = stack.Pop();
+                    racks++;
+                }
+            }
+            Console.WriteLine(racks);
+
+
+
+        }
+    }
+}
