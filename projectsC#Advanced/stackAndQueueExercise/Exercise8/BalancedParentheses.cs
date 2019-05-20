be67ee7515f6bc8199374a0fd1005ffab9dd7f0c
BudinKolyandov <budin_kolyandov@abv.bg>
2019-05-20 17:17:02 +0300

    Solved exercises 8 and 9.

---
+++ projectsC#Advanced/stackAndQueueExercise/Exercise8/BalancedParentheses.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise8
+{
+    class BalancedParentheses
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            bool isValid = true;
+            char[] valid = { '(', '[', '{' };
+                       
+            foreach(var i in input)
+            {
+                if (valid.Contains(i))
+                {
+                    stack.Push(i);
+                    continue;
+                }
+                if (stack.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+
+                if (stack.Peek() == '(' && i == ')')
+                {
+                    stack.Pop();
+                }
+                else if (stack.Peek() == '{' && i == '}')
+                {
+                    stack.Pop();
+                }
+                else if (stack.Peek() == '[' && i == ']')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    isValid = false;
+                    break;
+                }
+
+            }
+            if (isValid)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+
+
+        }
+    }
+}
---
+++ projectsC#Advanced/stackAndQueueExercise/Exercise9/SimpleTextEditor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise9
+{
+    class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> commandsMade = new Stack<string>();
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder text = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (input[0] == "1")
+                {
+                    commandsMade.Push(text.ToString());
+                    text.Append(input[1]);
+                }
+                if (input[0] =="2")
+                {
+                    int count = int.Parse(input[1]);
+                    commandsMade.Push(text.ToString());
+                    text.Remove(text.Length - count, count);
+                }
+                if (input[0] == "3")
+                {
+                    int count = int.Parse(input[1]);
+                    Console.WriteLine(text[count - 1]);
+                }
+                if (input[0] == "4")
+                {
+                    text.Clear();
+                    text.Append(commandsMade.Pop());
+                }
+            }
+
+
+
+        }
+    }
+}
