6786571c49e0f626f8d5ef42fae23a163882973a
BudinKolyandov <budin_kolyandov@abv.bg>
2019-05-21 18:49:37 +0300

    Finished exercises 5 and 6

---
+++ projectsC#Advanced/MultidimensionalArrays/MultidimensionalArraysExercise5/SnakeMoves.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MultidimensionalArraysExercise5
+{
+    class SnakeMoves
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            char[] snake = Console.ReadLine().ToCharArray();
+            Queue<char> queue = new Queue<char>(snake);
+            char[,] array = new char[size[0], size[1]];
+            for (int i = 0; i < size[0]; i++)
+            {
+                for (int j = 0; j < size[1]; j++)
+                {
+                    if (snake.Length == 0)
+                    {
+                        continue;
+                    }
+                    if (queue.Count >= size[1])
+                    {
+                        array[i, j] = queue.Dequeue();
+                    }
+                    else
+                    {
+                        foreach (var item in snake)
+                        {
+                            queue.Enqueue(item);
+                        }
+                        array[i, j] = queue.Dequeue();
+                    }
+                }
+            }
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int col = 0; col < array.GetLength(1); col++)
+                {
+                    Console.Write("{0}", array[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
---
+++ projectsC#Advanced/MultidimensionalArrays/MultidimensionalArraysExercise6/BombtheBasement.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace MultidimensionalArraysExercise6
+{
+    class BombtheBasement
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[,] array = new int [size[0], size[1]];
+            for (int i = 0; i < size[0]; i++)
+            {
+                for (int j = 0; j < size[1]; j++)
+                {
+                    array[i, j] = 0;
+                }
+            }
+            int[] cordinates = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int targetRow = cordinates[0];
+            int targetCol = cordinates[1];
+            int radius = cordinates[2];
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int col = 0; col < array.GetLength(1); col++)
+                {
+                    bool isInRadius = Math.Pow(row - targetRow, 2) + 
+                        Math.Pow(col - targetCol, 2) <= Math.Pow(radius, 2);
+                    if (isInRadius)
+                    {
+                        array[row, col] = 1;
+                    }
+                }
+            }
+            for (int col = 0; col < array.GetLength(1); col++)
+            {
+                int count = 0;
+                for (int row = 0; row < array.GetLength(0); row++)
+                {
+                    if (array[row, col] == 1)
+                    {
+                        count++;
+                        array[row, col] = 0;
+                    }
+                }
+                for (int row = 0; row < count; row++)
+                {
+                    array[row, col] = 1;
+                }
+            }
+
+
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int col = 0; col < array.GetLength(1); col++)
+                {
+                    Console.Write("{0}", array[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
