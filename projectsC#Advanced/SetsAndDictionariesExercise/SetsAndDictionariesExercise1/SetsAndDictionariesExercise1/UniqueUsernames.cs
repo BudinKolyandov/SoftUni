2082cc13a6a94e07255ee86ce0c5ade0b2f38f7a
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-05-24 09:50:30 +0300

    Completed exercise 1 from Sets and Dictionaries.

---
+++ projectsC#Advanced/SetsAndDictionariesExercise/SetsAndDictionariesExercise1/SetsAndDictionariesExercise1/UniqueUsernames.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetsAndDictionariesExercise1
+{
+    class UniqueUsernames
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> names = new Dictionary<int, string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                if (!names.ContainsValue(name))
+                {
+                    names.Add(i, name);
+                }
+
+            }
+            foreach (var kvp in names)
+            {
+                Console.WriteLine(kvp.Value);
+            }
+
+        }
+    }
+}
