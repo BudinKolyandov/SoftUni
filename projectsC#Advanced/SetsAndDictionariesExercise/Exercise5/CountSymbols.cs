c1e8d8291d761468fe7490f7563608b7fb2b05ce
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-05-25 09:34:24 +0300

    Finished exercise 5

---
+++ projectsC#Advanced/SetsAndDictionariesExercise/Exercise5/CountSymbols.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise5
+{
+    class CountSymbols
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach (var ch in text)
+            {
+                if (!dict.ContainsKey(ch))
+                {
+                    dict[ch] = 1;
+                }
+                else
+                {
+                    dict[ch] += 1;
+                }
+            }
+            foreach (var kvp in dict.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+
+
+
+        }
+    }
+}
