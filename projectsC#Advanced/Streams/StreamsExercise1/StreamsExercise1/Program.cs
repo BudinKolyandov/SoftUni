811cfd5014ea668bce7f11ba9a57bf87a33b3734
BudinKolyandov <budin_kolyandov@abv.bg>
2019-05-31 23:54:47 +0300

    Completed Tasks 1 and 2 from Streams exercises.

---
+++ projectsC#Advanced/Streams/StreamsExercise1/StreamsExercise1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace StreamsExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathString = "../../../text.txt";
+            int count = 0;
+            StreamReader sr = new StreamReader(pathString);
+            using (sr)
+            {
+                string currentLine = sr.ReadLine();
+                while (currentLine != null)
+                {
+                    string replaced = currentLine.Replace('-', '@').Replace(',','@').Replace('.','@')
+                        .Replace('!', '@').Replace('?', '@');
+                    if (count % 2 == 0)
+                    {             
+                        string[] newLine = replaced.Split().Reverse().ToArray();
+                        Console.WriteLine(string.Join(' ', newLine));
+
+                    }
+                    currentLine = sr.ReadLine();
+                    count++;
+                }                    
+            }
+        }
+    }
+}
---
+++ projectsC#Advanced/Streams/StreamsExercise1/StreamsExercise2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace StreamsExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathString = "../../../text.txt";
+            int count = 1;
+            int letters = 0;
+            int punctuations = 0;
+            StreamReader sr = new StreamReader(pathString);
+            using (sr)
+            {
+                string currentLine = sr.ReadLine();
+                while (currentLine != null)
+                {
+                    for (int i = 0; i < currentLine.Length; i++)
+                    {
+                        if (currentLine[i] == ' ')
+                        {
+                            continue;
+                        }
+                        else if (currentLine[i] == '-' || currentLine[i] == '.'|| currentLine[i] == ',' ||
+                            currentLine[i] == '\'' || currentLine[i] == '!' || currentLine[i] == '?')
+                        {
+                            punctuations++;
+                        }
+                        else
+                        {
+                            letters++;
+                        }
+                    }
+                    Console.WriteLine($"Line {count}: {currentLine} ({letters})/({punctuations})");
+                    letters = 0;
+                    punctuations = 0;
+
+                    currentLine = sr.ReadLine();
+                    count++;
+                }
+            }
+        }
+    }
+}
