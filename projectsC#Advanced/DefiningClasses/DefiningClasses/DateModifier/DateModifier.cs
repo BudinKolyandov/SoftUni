d12db70c61815dffb30fba4099210447f18cd8d1
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-06-08 19:27:51 +0300

    Completed problems 4 and 5.

---
+++ projectsC#Advanced/DefiningClasses/DefiningClasses/DateModifier/DateModifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class DateModifier
+    {
+        public static double  DifferenceBetweenDates(string dateFirst, string dateSecond)
+        {
+            DateTime firstDate = DateTime.ParseExact(dateFirst, "yyyy MM dd", CultureInfo.CurrentCulture);
+            DateTime secondDate = DateTime.ParseExact(dateSecond, "yyyy MM dd", CultureInfo.CurrentCulture);
+
+            if (secondDate < firstDate)
+            {
+                return DifferenceBetweenDates(dateSecond, dateFirst);
+            }
+
+            return (secondDate - firstDate).Days;
+            
+        }
+
+
+    }
+}
---
+++ projectsC#Advanced/DefiningClasses/DefiningClasses/OpinionPoll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] person = Console.ReadLine().Split();
+                string name = person[0];
+                int age = int.Parse(person[1]);
+
+                Person member = new Person(name, age);
+                people.Add(member);
+            }
+
+            foreach (var person in people.Where(x=>x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+
+        }
+    }
+}
