2875d1b500dcc7fc8ba2d3d32ec8ea420827765b
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-06-10 19:52:30 +0300

    Finished problem 6 form Defining Classes exercises!

---
+++ projectsC#Advanced/DefiningClasses/DefiningClasses/SpeedRacing/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        public Car(string model, double fuel, double consumption)
+        {
+            Model = model;
+            Fuel = fuel;
+            Consumption = consumption;
+
+            Distance = 0;
+        }
+
+        public string Model { get; set; }
+
+        public double Fuel { get; set; }
+
+        public double Consumption { get; set; }
+
+        public double Distance { get; set; }
+        
+        public void Drive(double distance)
+        {
+            double requiredFuel = distance * Consumption;
+            if (requiredFuel <= Fuel)
+            {
+                Distance += distance;
+                Fuel -= requiredFuel;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+    }
+}
---
+++ projectsC#Advanced/DefiningClasses/DefiningClasses/SpeedRacing/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using SpeedRacing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompanyRoster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> carsList = new List<Car>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cars = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string model = cars[0];
+                double fuel = double.Parse(cars[1]);
+                double consumption = double.Parse(cars[2]);
+
+                Car car = new Car(model, fuel, consumption);
+                carsList.Add(car);
+            }
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] data = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string model = data[1];
+                double distance = double.Parse(data[2]);
+
+                Car car = carsList.FirstOrDefault(x => x.Model == model);
+                car.Drive(distance);
+            }
+
+            foreach (Car car in carsList)
+            {
+                Console.WriteLine($"{car.Model} {car.Fuel:f2} {car.Distance}");
+            }
+
+        }
+    }
+}
