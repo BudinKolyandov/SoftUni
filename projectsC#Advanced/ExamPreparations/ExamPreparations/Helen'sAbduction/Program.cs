58e77fc5be5b759b13e16c2483034d365ab3912b
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-06-22 12:53:50 +0300

    Completed Helen's Abduction and Trojan Invasion from Retake Exam

---
+++ projectsC#Advanced/ExamPreparations/ExamPreparations/Helen'sAbduction/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Text;
+
+namespace Helen_sAbduction
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            int rows = int.Parse(Console.ReadLine());
+
+            char[][] field = new char[rows][];
+            bool won = false;
+            for (int i = 0; i < rows; i++)
+            {
+                field[i] = Console.ReadLine().ToCharArray();
+            }
+            int parisRow = 0;
+            int parisCol = 0;
+            for (int row = 0; row < field.Length; row++)
+            {
+                for (int col = 0; col < field[row].Length; col++)
+                {
+                    if (field[row][col] == 'P')
+                    {
+                        parisRow = row;
+                        parisCol = col;
+                    }
+                }
+            }
+
+            while (energy > 0)
+            {
+                string[] commands = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string direction = commands[0].ToLower();
+                int enemyRow = int.Parse(commands[1]);
+                int enemyCol = int.Parse(commands[2]);
+
+                field[enemyRow][enemyCol] = 'S';
+
+                energy--;
+
+                switch (direction)
+                {
+                    case "up":
+                        if (parisRow - 1 >= 0)
+                        {
+                            field[parisRow][parisCol] = '-';
+                            parisRow--;
+                        }
+                        break;
+                    case "down":
+                        if (parisRow + 1 < field.Length)
+                        {
+                            field[parisRow][parisCol] = '-';
+                            parisRow++;
+                        }
+                        break;
+                    case "left":
+                        if (parisCol - 1 >= 0)
+                        {
+                            field[parisRow][parisCol] = '-';
+                            parisCol--;
+                        }
+                        break;
+                    case "right":
+                        if (parisCol + 1 < field[parisRow].Length)
+                        {
+                            field[parisRow][parisCol] = '-';
+                            parisCol++;
+                        }
+                        break;
+                }
+
+                if (field[parisRow][parisCol] == 'S')
+                {
+                    energy -= 2;
+                    if (energy <=0)
+                    {
+                        field[parisRow][parisCol] = 'X';
+                        break;
+                    }
+                    else
+                    {
+                        field[parisRow][parisCol] = 'P';
+                    }
+                }
+
+                if (field[parisRow][parisCol] == 'H')
+                {
+                    won = true;
+                    field[parisRow][parisCol] = '-';
+                    break;
+                }
+
+                if (field[parisRow][parisCol] == '-')
+                {
+                    if (energy<=0)
+                    {
+                        field[parisRow][parisCol] = 'X';
+                    }
+                    else
+                    {
+                        field[parisRow][parisCol] = 'P';
+                    }
+                    
+                }
+            }
+
+            if (won)
+            {
+                Console.WriteLine($"Paris has successfully abducted Helen! Energy left: {energy}");
+                for (int row = 0; row < field.Length; row++)
+                {
+                    for (int col = 0; col < field[row].Length; col++)
+                    {
+                        Console.Write(field[row][col]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Paris died at {parisRow};{parisCol}.");
+                for (int row = 0; row < field.Length; row++)
+                {
+                    for (int col = 0; col < field[row].Length; col++)
+                    {
+                        Console.Write(field[row][col]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ projectsC#Advanced/ExamPreparations/ExamPreparations/TrojanInvasion/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrojanInvasion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            int waves = int.Parse(Console.ReadLine());
+            int[] platesInput = Console.ReadLine()
+                .Split()                
+                .Select(int.Parse)
+                .ToArray();
+            List<int> plates = new List<int>(platesInput);
+
+            for (int i = 1; i <= waves; i++)
+            {
+                int[] currentWave = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                if (i % 3 == 0)
+                {
+                    plates.Add(int.Parse(Console.ReadLine()));
+                }
+                Stack<int> warriors = new Stack<int>(currentWave);
+                
+                while (warriors.Count > 0 && plates.Count > 0)
+                {
+                    int currentWarrior = warriors.Pop();
+                    int currentPlate = plates[0];
+
+                    if (currentWarrior > currentPlate )
+                    {
+                        currentWarrior -= currentPlate;
+                        warriors.Push(currentWarrior);
+                        plates.RemoveAt(0);
+                    }
+                    else if(currentWarrior < currentPlate)
+                    {
+                        currentPlate -= currentWarrior;
+                        plates[0] = currentPlate;
+                    }
+                    else
+                    {
+                        plates.RemoveAt(0);
+                    }
+
+
+                    if (plates.Count == 0)
+                    {
+                        Console.WriteLine("The Trojans successfully destroyed the Spartan defense.");
+                        if (warriors.Count > 0)
+                        {
+                            Console.WriteLine($"Warriors left: {string.Join(", ", warriors)}");
+                        }
+                        Environment.Exit(0);
+                    }
+                    
+                }
+
+            }
+            Console.WriteLine("The Spartans successfully repulsed the Trojan attack.");
+            if (plates.Count > 0)
+            {
+                Console.WriteLine($"Plates left: {string.Join(", ", plates)}");
+            }
+            Environment.Exit(0);
+
+        }
+    }
+}
