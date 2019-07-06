a90d1dfaec1b6748bca3c0892b5432b29eeefd4d
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-07-06 16:21:37 +0300

    Completed exercises 1-3 from Inheritance - Exercise.

---
+++ C#OOP/02-Inheritance/PlayersAndMonsters/DarkKnight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    class DarkKnight : Knight
+    {
+
+        public DarkKnight(string username, int level)
+            : base(username, level)
+        {
+
+        }
+
+    }
+}
---
+++ C#OOP/02-Inheritance/PlayersAndMonsters/Knight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    class Knight : Hero
+    {
+
+        public Knight(string username, int level)
+            : base(username, level)
+        {
+
+        }
+
+    }
+}
