0488dd674ef8604a58e2c0f8fb0cdfe977e69d61
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-07-11 14:37:50 +0300

    Completed exercise 1 from Encapsulation - Exercise

---
+++ C#OOP/03-Encapsulation/ExercisesEncapsulation/ClassBoxData/Box.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClassBoxData
+{
+    class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double wigth, double height)
+        {
+            this.Length = length;
+
+            this.Width = wigth;
+
+            this.Height = height;
+                        
+        }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                this.length = value;
+            }
+        }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }        
+        }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            double surfaceArea = 2 * Length * Width + 2 * Length * Height + 2 * Width * Height;
+            return surfaceArea;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            double lateralSurfaceArea = 2 * Length * Height + 2 * Width * Height;
+            return lateralSurfaceArea;
+        }
+
+        public double Volume()
+        {
+            double volume = Length * Height * Width;
+            return volume;
+        }
+
+
+
+    }
+}
---
+++ C#OOP/03-Encapsulation/ExercisesEncapsulation/ClassBoxData/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                double surfaceArea = box.SurfaceArea();
+                double lateralSurfaceArea = box.LateralSurfaceArea();
+                double volume = box.Volume();
+
+                Console.WriteLine($"Surface Area - {surfaceArea:f2}");
+                Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
+                Console.WriteLine($"Volume - {volume:f2}");
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+
+        }
+    }
+}
