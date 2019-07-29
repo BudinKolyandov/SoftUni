bf421c8357e6e1dbc1560664e6c812dd6341b0f0
Budin Kolyandov <budin_kolyandov@abv.bg>
2019-07-29 17:16:55 +0300

    Completed Reflection and Attributes exercise.

---
+++ C#OOP/Reflection-and-Attributes/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,31 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    class CommandInterpreter : ICommandInterpreter
+    {
+        private const string command = "Command";
+        public string Read(string args)
+        {
+            string[] tokens = args
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string commandName = tokens[0] + command;
+            string[] commandArgs = tokens.Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type[] types = assembly.GetTypes();
+            Type typeToCreate = types.FirstOrDefault(t => t.Name == commandName);
+            Object instance = Activator.CreateInstance(typeToCreate);
+            ICommand currentCommand = (ICommand)instance;
+            string result = currentCommand.Execute(commandArgs);
+            return result;
+
+        }
+    }
+}
---
+++ C#OOP/Reflection-and-Attributes/ValidationAttributes/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class Person
+    {
+        public Person(string fullName, int Age)
+        {
+            this.FullName = fullName;
+            this.Age = Age;
+        }
+        [MyRequired]
+        public string FullName { get; set; }
+        [MyRange(12, 90)]
+        public int Age { get; set; }
+    }
+}
